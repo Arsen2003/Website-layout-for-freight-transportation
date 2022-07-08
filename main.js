9d0cd053be33e99a5c2e101483c5ed5d9493f2c4
Arsen2003 <arsen_nurdavletov@mail.ru>
2022-07-08 16:49:11 +0600

    hero content

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+const heroBackgr = document.getElementById('heroS')
+
+
+
+const heroBackgrArr = ['.icons\hero-img\hero.png' , 'icons\hero-img\Без названия (2).png']
+
+function back(){
+  for(let i = 0;i < heroBackgrArr;i++){
+i++
+ heroBackgrArr[i] = i
+    let ghr = document.section.style.background
+  }
+
+}
+
+const nextBtn = document.getElementById('btnN').style.background = heroBackgrArr[0]
